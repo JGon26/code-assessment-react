1aaef45b8df400e328445afddc185adc84bde5a2
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2022-02-27 14:54:20 -0800

    Downloaded axios, imported useEffect/useState to save list retreieved from mockaroo api on page load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [truckList, updateTruckList] = useState([]);
+
+  useEffect(()=> {
+    axios({
+      method: 'get',
+      url: 'https://my.api.mockaroo.com/locations.json?key=a45f1200'
+    })
+      .then((result) => updateTruckList(result.data))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
     </div>
   );
 }
